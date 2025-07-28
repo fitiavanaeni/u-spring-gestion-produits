ab359f6e5e34dd19d5381eac8d703236c82a4ef5
Fitiavana2 <fitiavanaeni@gmail.com>
2025-07-28 18:45:29 +0300

    add comandLindLinner and projection

--- src/main/java/com/fitiavana/produits/restcontrollers/produitRESTController.java
+++ src/main/java/com/fitiavana/produits/restcontrollers/produitRESTController.java
@@ -3,10 +3,7 @@ package com.fitiavana.produits.restcontrollers;
 import com.fitiavana.produits.entities.Produit;
 import com.fitiavana.produits.services.ProduitService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,7 +16,31 @@ public class produitRESTController {
     private ProduitService produitService;
 
     @RequestMapping(method = RequestMethod.GET)
-    List<Produit> getAllProduits(){
+    public List<Produit> getAllProduits(){
         return produitService.getAllProduits();
     }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Produit getProduitById(@PathVariable("id")  Long id){
+        return produitService.getProduit(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Produit createProduit(@RequestBody Produit produit){
+        return produitService.saveProduit(produit);
+    }
+    @RequestMapping(method = RequestMethod.PUT)
+    public Produit updateProduit(@RequestBody Produit produit){
+        return produitService.updateProduit(produit);
+    }
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void deleteProduit(@PathVariable("id") Long id){
+        produitService.deleteProduitById(id);
+    }
+    @RequestMapping(value = "/procat/{idCat}", method = RequestMethod.GET)
+    public List<Produit> getProduitsByCatId(@PathVariable("idCat") Long idCat){
+        return produitService.findByCategorieIdCat(idCat);
+    }
+
+
 }
